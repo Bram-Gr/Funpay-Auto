db8c26b7f0e64e8715f94d9ac2df3921f7d2b1cc
Vas <v.volkov.@gmail.com>
2024-09-01 16:53:58 +0300

    Login screen concept done moving onto button func with selenium

--- main.py
+++ main.py
@@ -3,6 +3,32 @@ import sv_ttk
 from tkinter import *
 from tkinter import ttk
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def clear_default_text(event):
+    if login_entry.get() == "Username":
+        login_entry.delete(0, END)
+
+def clear_password_text(event):
+    if password_entry.get() == "23423":
+        password_entry.delete(0, END)
+
 root = tkinter.Tk() 
 root.title("Funpay Automation")  # Set the window title
 
@@ -16,14 +42,20 @@ loginLabel.grid(row=0, column=1, pady=10)
 
 login_entry = ttk.Entry(mainframe, exportselection=0, width=30, font=("Arial", 15))
 login_entry.grid(row=1, column=1, padx=10)  # Decreased pady to 2
+login_entry.insert(0, "Username")  # Set default text
+login_entry.bind("<FocusIn>", clear_default_text)  # Bind the function to the FocusIn event
 
 password_entry = ttk.Entry(mainframe, show="*", exportselection=0, width=30, font=("Arial", 15))
 password_entry.grid(row=2, column=1, pady=10, padx=10)
-password_entry.focus_set()
+password_entry.insert(0, "23423")  # Set default text
+password_entry.bind("<FocusIn>", clear_password_text)  # Bind the function to the FocusIn event
+
+# Set transparency
+login_entry.configure(foreground="gray50")
+login_entry.configure(foreground="gray50")
 
 # Center the mainframe in the window
 mainframe.place(relx=0.5, rely=0.5, anchor=CENTER)
-
 
 sv_ttk.set_theme("dark")
 
